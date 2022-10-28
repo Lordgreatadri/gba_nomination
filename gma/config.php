ce0e4ef2a5e4549bed4d45bb73cea5f6a6c93c62
Emmanuel Lordgreat-Adri <rexmerlo@gmail.com>
2022-10-28 07:32:38 -0700

    Update config.php

--- gma/config.php
+++ gma/config.php
@@ -2,7 +2,7 @@
     $serverName       = "127.0.0.1";
     $databaseName     = "presby_ypg"; //
     $databaseUser     = "root";
-    $databasePassword = '#4kLxMzGurQ7Z~'; #"#4kLxMzGurQ7Z~"; Mccg8(3P^tJVnBDsF
+    $databasePassword = '#v~'; #"#v~"; v
 
     $database = mysqli_connect($serverName, $databaseUser, $databasePassword, $databaseName);
 
@@ -12,8 +12,8 @@
 
     $db_host = '127.0.0.1';
     $db_name = 'presby_ypg';//
-    $db_username = 'root';//Mccg8(3P^tJVnBDsF
-    $pass_word = '#4kLxMzGurQ7Z~';//$databasePassword;//'Mccg8(3P^tJVnBDsF';//#4kLxMzGurQ7Z~
+    $db_username = 'root';//v(3P^v
+    $pass_word = '#v~';//$databasePassword;//'v(3P^v';//#4kLxMzGurQ7Z~
     $charset = 'utf8mb4';
     $server_path = "mysql:host=".$db_host.";dbname=".$db_name.";charset=".$charset;
     $connect = new PDO($server_path, $db_username, $pass_word);//"mysql:host=localhost;dbname=behind_voice;charset=utf8","root",""
