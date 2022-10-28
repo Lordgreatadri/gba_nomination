ef2f404313da3c398a7f7815b1c0b24708283287
Emmanuel Lordgreat-Adri <rexmerlo@gmail.com>
2022-10-28 07:35:45 -0700

    Update DbCon.php

--- gma/nomination/category/controllers/classes/DbCon.php
+++ gma/nomination/category/controllers/classes/DbCon.php
@@ -4,10 +4,10 @@
 
     class DbCon
     {
-        private $db_host = '127.0.0.1';//192.168.193.254
+        private $db_host = '127.0.0.1';// 
         private $db_name = 'makeup_awards';//mycloud
         private $db_username = 'root';
-        private $pass_word = '#4kLxMzGurQ7Z~';//adRi@1234&5$HaW9(1&Mcc
+        private $pass_word = '# ~';//a 
         private $charset = 'utf8mb4';
 
         //access to server/database connection 2018-04-09 16:02:44
