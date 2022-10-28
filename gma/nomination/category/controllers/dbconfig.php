6bbc12601e4613f0ea02a5d7391abfee50830673
Emmanuel Lordgreat-Adri <rexmerlo@gmail.com>
2022-10-28 07:35:12 -0700

    Update dbconfig.php

--- gma/nomination/category/controllers/dbconfig.php
+++ gma/nomination/category/controllers/dbconfig.php
@@ -3,8 +3,7 @@
    $serverName = "127.0.0.1";
     $databaseName = "makeup_awards"; 
     $databaseUser = "root";
-    $databasePassword = "#4kLxMzGurQ7Z~";//#4kLxMzGurQ7Z" ;//''; //Bim32!@b)o #4kLxMzGurQ7Z~
-
+    $databasePassword = "# ~";//# " ;//''; // 
     $conn = mysqli_connect($serverName, $databaseUser, $databasePassword, $databaseName);
 
     if (!$conn) {
